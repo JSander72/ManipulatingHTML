f706653803cc373abcb74d3041e625a8dbea58db
JSander72 <jamesearlsanders2@outlook.com>
2023-06-19 17:52:48 -0400

    second commit

--- jQuery/appJQ.js
+++ jQuery/appJQ.js
@@ -1,6 +1,1 @@
-$(document). ready(function(){
-  $('#DynamicDiv'). append('<input type="button" id="dynamic" value="Dynamic Button" />');
-  $('#clickMe'). click(function () {
-  alert("Hey I am Static Control");
-  });
-});
+
