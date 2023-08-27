b7ae36d731e3435bb1e4b1d2786d731f609729f0
JSander72 <jamesearlsanders2@outlook.com>
2023-08-27 13:11:35 -0400

    second commit

--- DOM/appDOM.js
+++ DOM/appDOM.js
@@ -10,20 +10,33 @@ document.addEventListener("DOMContentLoaded", function () {
   button.addEventListener("click", function () {
       // Display an alert when the button is clicked
       alert("Nice!");
-      // Function to display the alert message
-      function showAlert() {
-        const messageInput = document.getElementById("messageInput");
-        const message = messageInput.value.trim(); // Get the value and trim whitespace
-
-        if (message) {
-            alert(message);
-        } else {
-            alert("Please enter a message.");
-        }
-      }
-
-      // Add a click event listener to the button
-      const showButton = document.getElementById("showButton");
-      showButton.addEventListener("click", showAlert);
   });
+  // Function to display the alert message
+  function showAlert() {
+    const messageInput = document.getElementById("messageInput");
+    const message = messageInput.value.trim(); // Get the value and trim whitespace
+
+    if (message) {
+        alert(message);
+    } else {
+        alert("Please enter a message.");
+    }
+  }
+
+  const showButton = document.getElementById("showButton");
+  showButton.addEventListener("click", showAlert);
+
+const colorChangeDiv = document.getElementById("colorChangeDiv");
+
+const originalColor = colorChangeDiv.style.backgroundColor;
+
+colorChangeDiv.addEventListener("mouseenter", () => {
+    colorChangeDiv.style.backgroundColor = "lightcoral";
+});
+
+colorChangeDiv.addEventListener("mouseleave", () => {
+    colorChangeDiv.style.backgroundColor = originalColor;
+});
+
+
 });
