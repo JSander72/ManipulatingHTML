1f3bef2df55abf898935b2fb4bc6ceba3129bbda
JSander72 <jamesearlsanders2@outlook.com>
2023-08-27 12:36:45 -0400

    first commit

--- DOM/appDOM.js
+++ DOM/appDOM.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Make a button
+  var button = document.createElement("button");
+  button.textContent = "Click me!";
 
+  // Append the button to body
+  document.body.appendChild(button);
+
+  // Add an event listener
+  button.addEventListener("click", function () {
+      // Display an alert when the button is clicked
+      alert("Nice!");
+      // Function to display the alert message
+      function showAlert() {
+        const messageInput = document.getElementById("messageInput");
+        const message = messageInput.value.trim(); // Get the value and trim whitespace
+
+        if (message) {
+            alert(message);
+        } else {
+            alert("Please enter a message.");
+        }
+      }
+
+      // Add a click event listener to the button
+      const showButton = document.getElementById("showButton");
+      showButton.addEventListener("click", showAlert);
+  });
+});
